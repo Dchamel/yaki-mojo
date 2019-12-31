9305e22822ee86593abb55724bfc7e19fc9732ea
Dchamel <dchamel@yandex.ru>
2019-12-31 04:36:01 +0400

    Ver 1.1
    Add view, some useful cols, change compare with input xml. Finished sunc store trade DB with DB Website now sear working prop.

--- mass_checkndo.php
+++ mass_checkndo.php
@@ -4,10 +4,10 @@
 require_once ("../config.php");
 require_once ('PHPExcel.php');
 require_once ('PHPExcel/IOFactory.php');
-require_once ("../func/photo.php"); //  фото-функции	
+require_once ("../func/photo.php"); //  С„РѕС‚Рѕ-С„СѓРЅРєС†РёРё	
 
-MYSQL_CONNECT($hostname,$username,$password) OR DIE("Не могу создать соединение ");
-@mysql_select_db("$dbName") or die("Не могу выбрать базу данных "); 
+MYSQL_CONNECT($hostname,$username,$password) OR DIE("РќРµ РјРѕРіСѓ СЃРѕР·РґР°С‚СЊ СЃРѕРµРґРёРЅРµРЅРёРµ ");
+@mysql_select_db("$dbName") or die("РќРµ РјРѕРіСѓ РІС‹Р±СЂР°С‚СЊ Р±Р°Р·Сѓ РґР°РЅРЅС‹С… "); 
 mysql_query("SET CHARACTER SET cp1251");
 mysql_query("SET NAMES cp1251");
 
@@ -16,38 +16,53 @@ $xls = PHPExcel_IOFactory::load('mass_checkndo.xls');
 $xls->setActiveSheetIndex(0);
 $sheet = $xls->getActiveSheet();
 
-// Обрабатываем и подготавливаем содержимое Икселя 
+// РћР±СЂР°Р±Р°С‚С‹РІР°РµРј Рё РїРѕРґРіРѕС‚Р°РІР»РёРІР°РµРј СЃРѕРґРµСЂР¶РёРјРѕРµ РРєСЃРµР»СЏ 
 
 echo '<table border="1">';
 $counter = 0;
 
-for ($i = 2; $i <= 220; $i++) {  // С какой по какую строку
+// Р—Р°РіРѕР»РѕРІРєРё
+echo '<tr>';
+echo '<td>в„–</td>';
+echo '<td>РРјСЏ</td>';
+echo '<td>РћРїРёСЃР°РЅРёРµ</td>';
+echo '<td>Р¦РµРЅР°</td>';
+echo '<td>Р’РµСЃ</td>';
+echo '<td>РљР°С‚РµРіРѕСЂРёСЏ - ID</td>';
+echo '<td>РљР°С‚РµРіРѕСЂРёСЏ - РќР°Р·РІР°РЅРёРµ</td>';
+echo '</tr>';
 
-    // $red_id //ИД блюда
+for ($i = 2; $i <= 257; $i++) {  // РЎ РєР°РєРѕР№ РїРѕ РєР°РєСѓСЋ СЃС‚СЂРѕРєСѓ
+
+    // $red_id //РР” Р±Р»СЋРґР°
     
-    //Запись самих данных 
-    $rusname = iconv("utf-8","windows-1251",$sheet->getCellByColumnAndRow(0, $i)->getValue());//Имя
-    $ves = iconv("utf-8","windows-1251",$sheet->getCellByColumnAndRow(1, $i)->getValue());//Вес
-    $rusprice = iconv("utf-8","windows-1251",$sheet->getCellByColumnAndRow(2, $i)->getValue());//Цена
-    $rusinfo = iconv("utf-8","windows-1251",$sheet->getCellByColumnAndRow(3, $i)->getValue());//Состав
-    
-    $showw = 1; //Показывать 0 или 1
-    $al_id = 143;  //ИД категории 139 в данном случае
+    //
+    $rusname = iconv("utf-8","windows-1251",$sheet->getCellByColumnAndRow(1, $i)->getValue());//РРјСЏ
+    $rusinfo = iconv("utf-8","windows-1251",$sheet->getCellByColumnAndRow(2, $i)->getValue());//РћРїРёСЃР°РЅРёРµ
+    $rusprice = iconv("utf-8","windows-1251",$sheet->getCellByColumnAndRow(3, $i)->getValue());//Р¦РµРЅР°
+    $ves = iconv("utf-8","windows-1251",$sheet->getCellByColumnAndRow(4, $i)->getValue());//Р’РµСЃ
+    $cat_num = iconv("utf-8","windows-1251",$sheet->getCellByColumnAndRow(5, $i)->getValue());//РљР°С‚РµРіРѕСЂРёСЏ ID
+    $cat_name = iconv("utf-8","windows-1251",$sheet->getCellByColumnAndRow(6, $i)->getValue());//РљР°С‚РµРіРѕСЂРёСЏ - РќР°Р·РІР°РЅРёРµ
+
+
+    $showw = 1; //РџРѕРєР°Р·С‹РІР°С‚СЊ 0 РёР»Рё 1
+    // $al_id = 143;  //РР” РєР°С‚РµРіРѕСЂРёРё 139 РІ РґР°РЅРЅРѕРј СЃР»СѓС‡Р°Рµ
     // $rusname=$ves;
 
         echo '<tr>';
       
-        // Столбцы
+        // РЎС‚РѕР»Р±С†С‹
         echo '<td>'. $i .'</td>';
         echo '<td>'. $rusname .'</td>';
         
-        //Поиск соответствия в БД и вывод
+        
+        //РџРѕРёСЃРє СЃРѕРѕС‚РІРµС‚СЃС‚РІРёСЏ РІ Р‘Р” Рё РІС‹РІРѕРґ
         echo '<td>';
         $zaoo = mysql_query("select * from gshir_pats WHERE pf_rusname='$rusname';");
         $raoo = mysql_fetch_array($zaoo);
             if ($raoo!=NULL) {
                 $counter++;
-                // $se = array("Суси", "Гункан");
+                // $se = array("РЎСѓСЃРё", "Р“СѓРЅРєР°РЅ");
                 // $newphrase = str_replace($se,'', $rusname);
                 // $zaoo2 = mysql_query("select * from gshir_pats WHERE pf_rusname LIKE lower('%$newphrase%');");
                 // $raoo2 = mysql_fetch_array($zaoo2);
@@ -57,24 +72,34 @@ for ($i = 2; $i <= 220; $i++) {  // С какой по какую строку
         echo '<br />';
         echo '</td>';
         
-
+        echo '<td>'. $rusprice .'</td>';
+        echo '<td>'. $ves .'</td>';
+        
         echo '</tr>';
 
+    // РЎР°РјРё Р·Р°РїСЂРѕСЃС‹
+    //Р—Р°РїРёСЃСЊ РІ РєР°С‚РµРіРѕСЂРёСЋ РІСЃРµС… С‚РѕРІР°СЂРѕРІ РёР· РРєСЃРµР»СЏ //`pf_rusinfo`='$rusinfo',`pf_show`='$showw',`al_id`='$al_id',
+    // $query = "INSERT INTO gshir_pats SET `pf_rusprice`='$rusprice',`pf_dataizm`=NOW() WHERE `pf_rusname`='$rusname';";
+    // $query = "UPDATE gshir_pats SET pf_rusprice='$rusprice' WHERE pf_rusname='$rusname';";
     
-    //Запись в категорию всех товаров из Икселя
-    // $query = "INSERT INTO gshir_pats SET `pf_rusname`='$rusname',`pf_rusinfo`='$rusinfo',`pf_show`='$showw',`al_id`='$al_id',`pf_rusprice`='$rusprice',`pf_ves`='$ves',`pf_dataizm`=NOW();";
-    
-    //Удаление из категории всех товаров
+    //РЈРґР°Р»РµРЅРёРµ РёР· РєР°С‚РµРіРѕСЂРёРё РІСЃРµС… С‚РѕРІР°СЂРѕРІ
     // $query ="DELETE FROM gshir_pats WHERE `al_id`='$al_id'";
     
-    // $result = mysql_query($query);
+    // Р’С‹РїРѕР»РЅРµРЅРёРµ Р·Р°РїСЂРѕСЃР° Р·Р°РїРёСЃРё
+    $result = mysql_query($query);
+    // if (mysql_query($query)) { 
+    //     echo "Record was updated successfully."; 
+    // } else { 
+    //     echo "ERROR: Could not able to execute $query. ". mysql_error(); 
+    // }  
+
     // $t_id=mysql_insert_id();
     // echo($t_id);
     
-    // Загрузка фото
-    // Если есть допфото 1 
+    // Р—Р°РіСЂСѓР·РєР° С„РѕС‚Рѕ
+    // Р•СЃР»Рё РµСЃС‚СЊ РґРѕРїС„РѕС‚Рѕ 1 
     // TransImg($_FILES["photo"]["tmp_name"],175,175,"../images/patslist/foto$t_id.jpg");
-    // Если есть фото-ОСНОВНОЕ 
+    // Р•СЃР»Рё РµСЃС‚СЊ С„РѕС‚Рѕ-РћРЎРќРћР’РќРћР• 
     // TransImg($_FILES["photo2"]["tmp_name"],800,800,"../images/patslist/bfoto$t_id.jpg");
     
     }
@@ -83,28 +108,47 @@ for ($i = 2; $i <= 220; $i++) {  // С какой по какую строку
     echo $counter;
 
 
-    // Показываем содержимое всего
-    echo "<p>----------- Содержимое загруженного и отображаемого. Взято из БД сайта. -----------</p>";
+    // РџРѕРєР°Р·С‹РІР°РµРј СЃРѕРґРµСЂР¶РёРјРѕРµ РІСЃРµРіРѕ
+    echo "<p>----------- РЎРѕРґРµСЂР¶РёРјРѕРµ Р·Р°РіСЂСѓР¶РµРЅРЅРѕРіРѕ Рё РѕС‚РѕР±СЂР°Р¶Р°РµРјРѕРіРѕ. Р’Р·СЏС‚Рѕ РёР· Р‘Р” СЃР°Р№С‚Р°. -----------</p>";
     $zaoo = mysql_query("select * from gshir_pats WHERE pf_show='1';");
     
 
     echo '<table border="1">';
+    
+        // Р—Р°РіРѕР»РѕРІРєРё
+        echo '<tr>';
+        echo '<td>в„–</td>';
+        echo '<td>РРјСЏ</td>';
+        echo '<td>РћРїРёСЃР°РЅРёРµ</td>';
+        echo '<td>Р¦РµРЅР°</td>';
+        echo '<td>Р’РµСЃ</td>';
+        echo '<td>РљР°С‚РµРіРѕСЂРёСЏ - ID</td>';
+        echo '<td>РљР°С‚РµРіРѕСЂРёСЏ - РќР°Р·РІР°РЅРёРµ</td>';
+        echo '</tr>';
+
     $counter=0;
      while($raoo = mysql_fetch_array($zaoo))
      {
        $counter++;
        
        $russname = $raoo['pf_rusname'];
+       $rusinfo = $raoo['pf_rusinfo'];
+       $rusprice = $raoo['pf_rusprice'];
+       $ves = $raoo['pf_ves'];
        $cat_num = $raoo['al_id'];
        
     //    echo($russname);
     //    echo("<br/>");	
-       
+
+        // РЎР°РјР° РёРЅС„РѕСЂРјР°С†РёСЏ
        echo '<tr>';
       
-       // Столбцы
+       // РЎС‚РѕР»Р±С†С‹
        echo '<td>'. $counter .'</td>';
        echo '<td>'. $russname .'</td>';
+       echo '<td>'. $rusinfo .'</td>';
+       echo '<td>'. $rusprice .'</td>';
+       echo '<td>'. $ves .'</td>';
        echo '<td>'. $cat_num .'</td>';
        $cat_name_arr = mysql_fetch_array(mysql_query("select * from gshir_patslist WHERE al_id='$cat_num';"));
        $cat_name = $cat_name_arr['al_rusname'];
